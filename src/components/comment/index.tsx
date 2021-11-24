09ae0ac680239f47e10cb889b7db9503880ff12a
Marcelo Lima <marcelo.lima@hotmart.com>
2021-11-24 12:49:08 -0300

    comments

--- src/components/comment/index.tsx
+++ src/components/comment/index.tsx
@@ -1,7 +1,26 @@
 import { ReactElement } from 'react';
+import { Image } from 'react-bootstrap';
 
 function Comment(): ReactElement {
-  return <p>comment</p>;
+  return (
+    <div className="comment">
+      <Image
+        height={28}
+        roundedCircle
+        src="https://www.redditstatic.com/avatars/defaults/v2/avatar_default_1.png"
+        width={28}
+      />
+      <div className="d-flex flex-column">
+        <p className="fs-6 fw-light">Anonymous user · 13:00</p>
+        <text>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto
+          at consequatur cum, debitis dolores ducimus est, hic illum, minus
+          nesciunt optio quod similique vel velit? Accusantium necessitatibus
+          porro suscipit?
+        </text>
+      </div>
+    </div>
+  );
 }
 
 export default Comment;
--- src/pages/thread-detail/index.tsx
+++ src/pages/thread-detail/index.tsx
@@ -1,6 +1,7 @@
 import { ReactElement } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 
+import Comment from '../../components/comment';
 import Thread from '../../components/thread';
 
 function ThreadDetail(): ReactElement {
@@ -12,8 +13,14 @@ function ThreadDetail(): ReactElement {
         </Col>
       </Row>
       <Row>
-        <Col xs={12}>
-          <div className="m-auto">teste</div>
+        <Col className="d-flex flex-row gap-2" xs={12}>
+          <Card>
+            <Card.Body>
+              <Comment />
+              <hr />
+              <Comment />
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
     </Container>
